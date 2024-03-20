cc8033c9fd5c91ba02ba78ba59be36652b8c1148
chududuchu <164136625+chududuchu@users.noreply.github.com>
2024-03-20 13:20:05 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("EL6McxmLqmsP8CSJhGFV83nwYrRH5Rwrt7rWekfUkTQA");
 
 #[program]
 pub mod nft_lend_borrow {
